4235bc70ce4dcba1dd52b46da1af0b4af188c6ae
Chinmay Kabi <chinmay.kabi@live.com>
2018-05-05 03:02:12 +0530

    Database is getting more complex
    User save option added, can resume from that point. Displaying the table of 5 at a time is being worked upon. Adios

--- app/src/main/java/surveyapp/thesmader/com/surveyapp/MainActivity.java
+++ app/src/main/java/surveyapp/thesmader/com/surveyapp/MainActivity.java
@@ -6,27 +6,42 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
-
+    public String subjectCode;
+    public String yearValue;
+    public String semesterValue;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
     }
+
+
     public void dataEntry(View view)
     {
        entryActivity x=new entryActivity();
 
         EditText editText=(EditText)findViewById(R.id.subject_code);
-        String message=editText.getText().toString();
+        String subjectCode=editText.getText().toString();
         Spinner year_select=(Spinner)findViewById(R.id.year_select);
-        message+=year_select.getSelectedItem().toString();
+        yearValue=year_select.getSelectedItem().toString();
         Spinner semester_select=(Spinner)findViewById(R.id.semester_select);
-        message+=semester_select.getSelectedItem().toString();
-        x.scode=message;
+        semesterValue=semester_select.getSelectedItem().toString();
+        x.scode=subjectCode;
+        x.semesterValue=semesterValue;
+        x.yearValue=yearValue;
+        if(yearValue.equals("Choose Year") || semesterValue.equals("Choose Semester"))
+            Toast.makeText(getApplicationContext(),"Please provide appropriate input", Toast.LENGTH_SHORT).show();
+        else
        startActivity(new Intent(this, entryActivity.class));
     }
 }
--- app/src/main/java/surveyapp/thesmader/com/surveyapp/entryActivity.java
+++ app/src/main/java/surveyapp/thesmader/com/surveyapp/entryActivity.java
@@ -1,26 +1,23 @@
 package surveyapp.thesmader.com.surveyapp;
 
-import android.app.NotificationManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,34 +27,57 @@ import java.util.Map;
  */
 
 public class entryActivity extends AppCompatActivity implements View.OnClickListener{
-   // EditText max = (EditText) findViewById(R.id.extra_paper_wastage);
-    public static String scode;
-    public String slno;
+   public static String scode;
+    public static String semesterValue;
+    public static String yearValue;
+    public static int marksValue;
+    public static int mainValue;
+    public int index;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     Map<String, Object> user = new HashMap<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_entry);
-        slno="0";
+        DocumentReference docRef = db.collection(scode).document("Last Accessed");
+
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if(document.exists()){
+                    if (document != null) {
+                        Log.d("", "DocumentSnapshot data: " + task.getResult().getData());
+                        String interimID =task.getResult().getData().get("Last Index").toString();
+                        if(interimID!=null)
+                            index=Integer.parseInt(interimID);
+                        //dataLabel.setText(userName);
+                    } else {
+                        Log.d("FirestoreDemo", "No such document");
+                    }
+                } else {
+                        Log.d("FirestoreDemo", "get failed with ", task.getException());
+                    }
+                }
+            }
+    });
     }
 
 public void onClick(View view)
 {
-
-   /* max.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-            if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
-               // Log.i(TAG,"Enter pressed"); */
                 EditText marks=(EditText)findViewById(R.id.marks_entry);
-                String markNote=marks.getText().toString();
+                marksValue=Integer.parseInt(marks.getText().toString());
                 EditText paper1=(EditText)findViewById(R.id.main_paper_wastage);
                 EditText paper2=(EditText)findViewById(R.id.extra_paper_wastage);
-                String wastage=paper1.getText().toString() + "+";
-                String s = wastage+ paper2.getText().toString();
-                user.put("marks",markNote);
+                mainValue=Integer.parseInt(paper1.getText().toString()) + Integer.parseInt(paper2.getText().toString());
+                user.put("Year",yearValue);
+                user.put("Semester",semesterValue);
+                user.put("marks",marksValue);
+                user.put("Wasted paper",mainValue);
+                user.put("Index",index++);
                 db.collection(scode)
-                       // .document(slno)
                         .add(user)
                         .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                             @Override
@@ -71,15 +91,57 @@ public void onClick(View view)
                                 Log.w("FirestoreDemo", "Error adding document", e);
                             }
                         });
-                slno=Integer.toString(Integer.parseInt(slno)+1);
-                Toast.makeText(getApplicationContext(),markNote+ " " + s, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),Integer.toString(mainValue), Toast.LENGTH_SHORT).show();
                 setContentView(R.layout.page_entry);
-                recreate();
-          /*  }
-            return false;
-        }
-    }); */
 
+
+}
+public void updateUI() {
+        if(index<5)
+    for (int i = 0; i <=index; i++) {
+        CollectionReference peopleRef=db.collection(scode);
+                peopleRef.whereEqualTo("Year", yearValue)
+                .whereEqualTo("Semester", semesterValue)
+                .whereEqualTo("Index", i)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (DocumentSnapshot document : task.getResult()) {
+                                Log.d("Yes", document.getId() + " => " + document.getData());// After this document.getString("Field name"), then link to respective TextView, done :D
+                            }
+                        } else {
+                            Log.d("No", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+}
+public void savingData(View view)
+{
+    user.put("Last Index", index);
+    user.put("Last Subject Code",scode);
+    user.put("Last Semester",semesterValue);
+    user.put("Last Year value",yearValue);
+    user.put("Index",-1);
+
+    db.collection(scode)
+            .document("Last Accessed")//Stores the credentials of the last worked upon tab
+            .set(user)
+            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Log.d("FirestoreDemo","Document snapshot added!");
+
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.w("FirestoreDemo","Error adding document",e);
+                }
+            });
 }
 public void goBack(View view)
 {
